b2b9673f92f9c64cbb0d3a305a0a6d3159028b36
Apurv Dugar <dugarapurv27@gmail.com>
2025-05-24 23:46:27 +0530

    Update dependencies in package.json and package-lock.json; removed unused packages and added support for additional SWC binaries.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Section2 from "@/components/Section2";
 import Section3 from "@/components/Section3";
 import Section4 from "@/components/Section4";
 import Footer from "@/components/Footer";
-import { TypeAnimation } from 'react-type-animation';
 
 export default function Home() {
   return (
