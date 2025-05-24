207fd0106e170bdc328adaf13cc17a6daa147d20
Mehul Agarwal <agarwalmehul423@gmail.com>
2025-05-24 19:23:05 +0530

    completed the basic look of dashboard home page

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -28,7 +28,7 @@ export default function DashboardLayout({
         <nav>
           <Navbar />
         </nav>
-        <main className="flex flex-row items-center">
+        <main className="flex flex-row">
           <Sidebar />
           {children}
         </main>
---
+++ app/dashboard/new-entry/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NewCapsuleForm = () => {
+  return (
+    <div className="flex flex-col max-w-4xl p-6 w-full mx-auto gap-6">
+      <div className="font-bold text-3xl">
+        Send a message to your future self:
+      </div>
+    </div>
+  );
+};
+
+export default NewCapsuleForm;
